9d517a3f0502ab080e7e7aaf8353a3c1bd67f591
JonKali <JonKali9@protonmail.com>
2021-04-20 16:40:06 +0200

    Got functionality working, now only need to add an API for the images.

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
+const formsDiv = document.getElementById("formsDiv");
+const userInput = document.querySelector('#input input');
+const goButton = document.querySelector('#input p');
+
+goButton.addEventListener('click', () => {
+  const city = userInput.value;
+  getWeather(city);
+})
+
 //Function to add Weather Data to document
 const setWeather = (city, country, weather, temperature, humidity, time) => {
-  console.log("City:", city);
-  console.log("Country:", country);
-  console.log("Weather:", weather);
-  console.log("Temperature:", temperature);
-  console.log("Humidity:", humidity);
-  console.log("Time:", time);
+  //Create form element
+  const form = document.createElement('div')
+  form.className = 'form';
+
+  //Create img element
+  const img = document.createElement('img');
+  img.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.ESzsUwoj0gC-s7qdLHV7zgHaEK%26pid%3DApi&f=1";
+  //Add img to form
+  form.appendChild(img);
+
+  //Create h2 element
+  const h2 = document.createElement('h2');
+
+  //Create span tags in h2
+  const cityP = document.createElement('span');
+  cityP.innerHTML = city + ", ";
+  const countryP = document.createElement('span');
+  countryP.innerHTML = country;
+  //Add span tags to h2
+  h2.appendChild(cityP);
+  h2.appendChild(countryP);
+  //Add h2 to form
+  form.appendChild(h2);
+
+  //Create weather element
+  const weatherP = document.createElement('p');
+  weatherP.innerHTML = weather;
+  form.appendChild(weatherP)
+  //Create temperature element
+  const tempP = document.createElement('p');
+  tempP.innerHTML = temperature;
+  form.appendChild(tempP)
+  //Create time element
+  const timeP = document.createElement('p');
+  timeP.innerHTML = time;
+  form.appendChild(timeP)
+
+  //Add form to forms
+  formsDiv.appendChild(form)
 }
 
 //Function to get Weather in a City
@@ -24,6 +66,7 @@ const getWeather = async city => {
     const response = await fetch(url, body);
     if (response.ok) {
       const jsonResponse = await response.json();
+      city = jsonResponse.name;
       const country = jsonResponse.sys.country;
       const weather = jsonResponse.weather[0].description;
       const temperature = jsonResponse.main.temp;
@@ -39,12 +82,3 @@ const getWeather = async city => {
     alert(error);
   }
 }
-
-/*
-//Call to get the Weather in New York
-getWeather('New York');
-//Call to get the Weather in London
-getWeather('London');
-//Call to get the Weather in Paris
-getWeather('Paris');
-*/
